2ff203c2f56217d6223ec18e54e6be2532c5fb2a
Setenta30 <gonzalo_britez@hotmail.com>
2024-05-07 02:09:32 -0300

    Added mockio and http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MaterialModule } from './shared/modules/material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { MaterialModule } from './shared/modules/material.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [
     provideAnimationsAsync()
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 import { IUser } from '../models/user';
 import { ILoginData } from '../models/login-data';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
+  private url = 'https://6639ac341ae792804bec6f8e.mockapi.io/api/login';
   private MOCK_AUTH_USER: IUser = {
     id: "1",
     email: "gonzalo.britez94@gmail.com",
@@ -21,15 +22,24 @@ export class AuthService {
   private _authUser$ = new BehaviorSubject<IUser | null>(null);
   public authUser$ = this._authUser$.asObservable();
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+  ) { }
 
   login(data: ILoginData): void {
+    this.http.get<ILoginData[]>(this.url).subscribe({
 
-    if (data.username === 'admin' && data.password === "1234") {
-      this._authUser$.next(this.MOCK_AUTH_USER)
-      localStorage.setItem('accessToken', '615ae670-9f54-4e9b-ae06-fea183b1d3ee');
-      this.router.navigate(['dashboard'])
-    }
+      next: (users: ILoginData[]) => {
+        const matchedUser = users.find(user => user.username === data.username && user.password === data.password);
+
+        if (matchedUser) {
+          this._authUser$.next(this.MOCK_AUTH_USER);
+          localStorage.setItem('accessToken', '615ae670-9f54-4e9b-ae06-fea183b1d3ee');
+          this.router.navigate(['dashboard']);
+        }
+      },
+    });
   }
 
   logout(): void {
